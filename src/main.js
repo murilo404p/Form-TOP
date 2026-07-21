0ec25080624ae2dfc72fd9e4752a2cedf404fb4b
Murilo Silva Papacidero <mpapacidero3@gmail.com>
2026-07-21 12:51:25 -0300

    Form finalizado com validacao de senhas iguais

--- src/main.js
+++ src/main.js
@@ -3,12 +3,15 @@ import './style.css'
 let password = document.getElementById("user_password");
 let confirmPassword = document.getElementById("confirm_password");
 let textConfirmPassword = document.getElementById("textConfirmPassword");
+let buttonConfirm = document.getElementById("buttonConfirm");
+let blankPassword = document.getElementById("blankPassword");
 
 confirmPassword.addEventListener("input", () => {
     if (password.value !== confirmPassword.value) {
-        ;
+        textConfirmPassword.classList.remove("hidden");
     } else {
-        textConfirmPassword.classList.add = ("hidden");
+        textConfirmPassword.classList.add("hidden");
     }
 });
+
 
