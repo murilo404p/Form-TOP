ff7da404c295602ff8eb9fce597cb8e150a417e7
Murilo Silva Papacidero <mpapacidero3@gmail.com>
2026-07-20 21:15:38 -0300

    fix: validar form para senha iguais

--- src/main.js
+++ src/main.js
@@ -1,2 +1,14 @@
 import './style.css'
 
+let password = document.getElementById("user_password");
+let confirmPassword = document.getElementById("confirm_password");
+let textConfirmPassword = document.getElementById("textConfirmPassword");
+
+confirmPassword.addEventListener("input", () => {
+    if (password.value !== confirmPassword.value) {
+        ;
+    } else {
+        textConfirmPassword.classList.add = ("hidden");
+    }
+});
+
