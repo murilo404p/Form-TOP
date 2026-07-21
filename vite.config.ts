62b6666c4c911b3d664963271838b6ec31bccb8b
Murilo Silva Papacidero <mpapacidero3@gmail.com>
2026-07-21 20:14:31 -0300

    add dist

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/Form-TOP',
+  base: '/Form-TOP/',
 
   plugins: [
     tailwindcss(),
